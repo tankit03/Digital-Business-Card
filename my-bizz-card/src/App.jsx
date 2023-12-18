6510add589f4280283bfae6a2117b8bd0e1519b2
tankit03 <huparetanish@gmail.com>
2023-12-17 19:56:44 -0800

    "Installed React package  made  changes - Tanish H

---
+++ my-bizz-card/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Intro from "./components/intro";
+
+export default function App () {
+
+    
+    return(
+        
+        <div>
+
+        <NavBar />
+        <Intro />
+        
+        </div>
+
+    )
+}
---
+++ my-bizz-card/src/components/intro.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Intro (){
+    return(
+        <main>
+
+            <h1 className="Intro--Name">Tanish Hupare</h1>
+            <h2 className="Intro--title">SWE engineer</h2>
+            <h3 className="Intro-Web">tanish.website</h3>
+
+        </main>
+    ) 
+        
+}
